8b65ee3805befd2ba6bf3b8d0a5002c6ceb198e7
Enderson Carvalho <enderson.s.carvalho@hotmail.com>
2025-01-25 15:32:21 -0300

    Update game.py

--- game.py
+++ game.py
@@ -3,111 +3,100 @@ from pygame.locals import *
 import sys
 
 pygame.init()
-pygame.display.set_caption("Kill Those Slimes, Cleitin!")
+pygame.display.set_caption("Mata os Slimes, Cleitin!")
 
 larg_tela = 800
 alt_tela = 600
-tela = pygame.display.set_mode((larg_tela, alt_tela)) #Observar para adaptar melhor o tamanho da tela
+tela = pygame.display.set_mode((larg_tela, alt_tela))
+BLACK = (0, 0, 0)
 
-WHITE = (255, 255, 255) #Lembrar de tirar depois, pra colocar o fundo do game mesmo
-
-def cut_sprite(sprite, frame_x, frame_y, num_frames, linha_frame):
+def cut_sprite(sprite, frame_x, frame_y, num_frames, num_l):
     animation = []
-    larg_sheet, alt_sheet = sprite.get_size()
-    for l in range(linha_frame):
+    for l in range(num_rows):
         for c in range(num_frames):
             x = c * frame_x
             y = l * frame_y
-            cut_frame = pygame.Rect(x, y, frame_x, frame_y)
-            frame = sprite.subsurface(cut_frame)
+            frame_rect = pygame.Rect(x, y, frame_x, frame_y)
+            frame = sprite.subsurface(frame_rect)
             animation.append(frame)
     return animation
 
-#Esperando os cortes para colocar as sprites
-sprite_sup = pygame.image.load("")
-sprite_sdown = pygame.image.load("")
-sprite_sleft = pygame.image.load("")
-sprite_sright = pygame.image.load("")
+run_up = pygame.image.load('Sprites/andandoc (1).png')
+run_down = pygame.image.load('Sprites/andandof (1).png')
+run_left = pygame.image.load('Sprites/andandole (1).png')
+run_right = pygame.image.load('Sprites/andandol (1).png')
 
-sprite_up = pygame.image.load("")
-sprite_down = pygame.image.load("")
-sprite_left = pygame.image.load("")
-sprite_right = pygame.image.load("")
+atk_up = pygame.image.load('Sprites/attackc.png')
+atk_down = pygame.image.load('Sprites/attackf.png')
+atk_right = pygame.image.load('Sprites/attackl.png')
 
-sprite_atk_up = pygame.image.load("")
-sprite_atk_down = pygame.image.load("")
-sprite_atk_left = pygame.image.load("")
-sprite_atk_right = pygame.image.load("")
+sup = pygame.image.load('Sprites/paradoc.png')
+sleft = pygame.image.load('Sprites/paradole.png')
+sdown = pygame.image.load('Sprites/paradof.png')
+sright = pygame.image.load('Sprites/paradol.png')
 
-sprite_dead = pygame.image.load("")
-
-#Sprite Slime
-sprite_up_slime = pygame.image.load("")
-sprite_down_slime = pygame.image.load("")
-sprite_left_slime = pygame.image.load("")
-sprite_right_slime = pygame.image.load("")
-
-sprite_dead_slime = pygame.image.load("")
-
-#parametros da animação, obs* lembrar de colocar os outros
 frame_x = 48
-frame_y = 48
-num_frames = 6
-linha_frame = 1
+frame_y = 58
+
+num_frames = 6 
+num_rows = 1
 atk_frames = 4
-dead_frames = 3
+stand_frames = 6
 
-stop_up = cut_sprite(sprite_sup, frame_x, frame_y, num_frames, linha_frame)
-stop_down = cut_sprite(sprite_sdown, frame_x, frame_y, num_frames, linha_frame)
-stop_left = cut_sprite(sprite_sleft, frame_x, frame_y, num_frames, linha_frame)
-stop_right = cut_sprite(sprite_sright, frame_x, frame_y, num_frames, linha_frame)
+player_run_up = cut_sprite(run_up, frame_x, frame_y, num_frames, num_rows)
+player_run_left = cut_sprite(run_left, frame_x, frame_y, num_frames, num_rows)
+player_run_down = cut_sprite(run_down, frame_x, frame_y, num_frames, num_rows)
+player_run_right = cut_sprite(run_right, frame_x, frame_y, num_frames, num_rows)
 
-run_up = cut_sprite(sprite_up, frame_x, frame_y, num_frames, linha_frame)
-run_down = cut_sprite(sprite_down, frame_x, frame_y, num_frames, linha_frame)
-run_left = cut_sprite(sprite_left, frame_x, frame_y, num_frames, linha_frame)
-run_right = cut_sprite(sprite_right, frame_x, frame_y, num_frames, linha_frame)
+player_atk_up = cut_sprite(atk_up, frame_x, frame_y, atk_frames, num_rows)
+player_atk_down = cut_sprite(atk_down, frame_x, frame_y, atk_frames, num_rows)
+player_atk_right = cut_sprite(atk_right, frame_x, frame_y, atk_frames, num_rows)
 
-atk_up = cut_sprite(sprite_atk_up, frame_x, frame_y, num_frames, linha_frame)
-atk_down = cut_sprite(sprite_atk_down, frame_x, frame_y, num_frames, linha_frame)
-atk_left = cut_sprite(sprite_atk_left, frame_x, frame_y, num_frames, linha_frame)
-atk_right = cut_sprite(sprite_atk_right, frame_x, frame_y, num_frames, linha_frame)
+player_stand_up_frames = cut_sprite(sup, frame_x, frame_y, stand_frames, num_rows)
+player_stand_left_frames = cut_sprite(sleft, frame_x, frame_y, stand_frames, num_rows)
+player_stand_down_frames = cut_sprite(sdown, frame_x, frame_y, stand_frames, num_rows)
+player_stand_right_frames = cut_sprite(sright, frame_x, frame_y, stand_frames, num_rows)
 
-death = cut_sprite(sprite_dead, frame_x, frame_y, num_frames, linha_frame)
+if not (player_run_up and player_run_left and player_run_down and player_run_right):
+    print("Erro: Nenhum quadro foi cortado das sprite sheets.")
+    pygame.quit()
+    sys.exit()
 
-#faltam alguns parâmetros, atualizar conforme faz
-pframe = 0
+player_width = 50
+player_height = 50
+
+player_x = (larg_tela - player_width) // 2
+player_y = (alt_tela - player_height) // 2
+
+player_speed = 2
+
+current_frame = 0
 frame_time = 100
 last_frame_time = pygame.time.get_ticks()
 
 last_direction = None
-is_atk = False
+is_attacking = False
 atk_frame_time = 100
-last_atk_time = pygame.time.get_ticks()
+last_attack_time = pygame.time.get_ticks()
 
-player_alt = 50
-player_larg = 50
-player_speed = 2
-
-player_x = (alt_tela - larg_tela) // 2
-player_y = (alt_tela - larg_tela) // 2
-
-#desenhar a sprite
-def play_animation(x, y, anime):
-    if pframe < len(anime):
-        tela.blit(anime[pframe], (x, y))
+def draw_player(x, y, animation_frames):
+    if current_frame < len(animation_frames):
+        tela.blit(animation_frames[current_frame], (x, y))
     else:
-        tela.blit(anime[0], (x, y))
+        tela.blit(animation_frames[0], (x, y))
 
-#loop principal, atualizar sempre
+
 clock = pygame.time.Clock()
-running =  True
+running = True
 while running:
-    tela.fill(WHITE)
+    tela.fill(BLACK)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
+
     moving = False
     if keys[pygame.K_a]:
         player_x -= player_speed
@@ -126,9 +115,72 @@ while running:
         last_direction = 'up'
         moving = True
 
+    if keys[pygame.K_SPACE] and not is_attacking:
+        is_attacking = True
+        current_frame = 0
+        last_attack_time = pygame.time.get_ticks()
+
+    if moving:
+        if last_direction == 'left':
+            if pygame.time.get_ticks() - last_frame_time > frame_time:
+                last_frame_time = pygame.time.get_ticks()
+                current_frame = (current_frame + 1) % len(player_run_left)
+            draw_player(player_x, player_y, player_run_left)
+
+        elif last_direction == 'right':
+            if pygame.time.get_ticks() - last_frame_time > frame_time:
+                last_frame_time = pygame.time.get_ticks()
+                current_frame = (current_frame + 1) % len(player_run_right)
+            draw_player(player_x, player_y, player_run_right)
+
+        elif last_direction == 'down':
+            if pygame.time.get_ticks() - last_frame_time > frame_time:
+                last_frame_time = pygame.time.get_ticks()
+                current_frame = (current_frame + 1) % len(player_run_down)
+            draw_player(player_x, player_y, player_run_down)
+
+        elif last_direction == 'up':
+            if pygame.time.get_ticks() - last_frame_time > frame_time:
+                last_frame_time = pygame.time.get_ticks()
+                current_frame = (current_frame + 1) % len(player_run_up)
+            draw_player(player_x, player_y, player_run_up)
+
+    elif is_attacking:
+        if pygame.time.get_ticks() - last_attack_time > atk_frame_time:
+            last_attack_time = pygame.time.get_ticks()
+            current_frame = (current_frame + 1) % len(player_atk_up)
+
+            if current_frame == 0:
+                is_attacking = False
+
+        if last_direction == 'up':
+            draw_player(player_x, player_y, player_atk_up)
+        elif last_direction == 'down':
+            draw_player(player_x, player_y, player_atk_down)
+        # elif last_direction == 'left':draw_player(player_x, player_y, player_atk_left)
+        elif last_direction == 'right':
+            draw_player(player_x, player_y, player_atk_right)
+
+    else:
+        if pygame.time.get_ticks() - last_frame_time > frame_time:
+            last_frame_time = pygame.time.get_ticks()
+            current_frame = (current_frame + 1) % len(player_stand_up_frames)
+
+        if last_direction == 'up':
+            tela.blit(
+                player_stand_up_frames[current_frame], (player_x, player_y))
+        elif last_direction == 'down':
+            tela.blit(
+                player_stand_down_frames[current_frame], (player_x, player_y))
+        elif last_direction == 'left':
+            tela.blit(
+                player_stand_left_frames[current_frame], (player_x, player_y))
+        elif last_direction == 'right':
+            tela.blit(
+                player_stand_right_frames[current_frame], (player_x, player_y))
 
     pygame.display.flip()
-    
+    clock.tick(60)
 
 pygame.quit()
 sys.exit()
